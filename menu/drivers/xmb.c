726134fbeb1dd306cac922e76e3e113c1fd276b3
alfrix <alfredomonclus@gmail.com>
2018-03-27 10:33:13 -0300

    Better scaling for left thumb

--- menu/drivers/xmb.c
+++ menu/drivers/xmb.c
@@ -3165,17 +3165,8 @@ static void xmb_frame(void *data, video_frame_info_t *video_info)
          msg_hash_to_str(MENU_ENUM_LABEL_VALUE_OFF)))
    {
 
-
-      float scale_factor =  (settings->uints.menu_xmb_scale_factor * 0.01);
-      if (scale_factor > 1.3)
-         scale_factor = scale_factor * 0.7;
-      else if ((scale_factor > 1 && scale_factor < 1.19) || (scale_factor < 1 && scale_factor > 0.81))
-         scale_factor = scale_factor * 0.9;
-      else if (scale_factor != 1)
-         scale_factor = scale_factor * 0.8;
-
-      left_thumb_width  = xmb->left_thumbnail_width * scale_factor;
-      left_thumb_height = xmb->left_thumbnail_height * scale_factor;
+      left_thumb_width  = xmb->left_thumbnail_width / scale_mod[4];
+      left_thumb_height = xmb->left_thumbnail_height / scale_mod[4];
 
       xmb_draw_thumbnail(video_info,
       xmb, &coord_white[0], width, height,
